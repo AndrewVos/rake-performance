a0dfc89df07758393fce2053cee88603f10a803f
AndrewVos <andrew.vos@gmail.com>
2011-01-30 19:30:20 +0000

    Added apeiros_'s code

--- lib/time_helper.rb
+++ lib/time_helper.rb
@@ -2,12 +2,10 @@ class TimeHelper
   class << self
     def time_difference(a, b)
       a, b = b, a if a < b
-      difference = Integer(a - b)
-      seconds = difference % 60
-      difference = (difference - seconds) / 60
-      minutes = difference % 60
-      difference = (difference - minutes) / 60
-      hours =  difference % 24
+      seconds = a - b
+      minutes, seconds = *seconds.divmod(60)
+      hours,   minutes = *minutes.divmod(60)
+      seconds = seconds.round
       result = [hours, minutes, seconds].map { |value| value.to_s.rjust(2, "0") }
       result.join(":")
     end
