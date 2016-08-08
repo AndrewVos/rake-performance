f2c63ed5d8f98eac89dd125cf39201f733806af8
Tim Callahan <tcallahan@covermymeds.com>
2016-08-08 17:38:06 -0400

    Time around the invoke with call chain as well, to capture timings of dependent tasks as well.

--- lib/rake_performance/task.rb
+++ lib/rake_performance/task.rb
@@ -3,6 +3,7 @@ require_relative 'time_helper'
 module Rake
   class Task
     alias :old_invoke :invoke
+    alias :old_invoke_with_call_chain :invoke_with_call_chain
 
     def invoke(*args)
       start_time = Time.now
@@ -10,7 +11,16 @@ module Rake
       old_invoke(*args)
       end_time = Time.now
       puts "Task '#{@name}' ended at #{end_time}"
-      puts "Total time taken: #{TimeHelper.time_difference(start_time, end_time)}" 
+      puts "Total time '#{@name}' took: #{TimeHelper.time_difference(start_time, end_time)}"
+    end
+
+    def invoke_with_call_chain(task_args, invocation_chain)
+      start_time = Time.now
+      puts "Task '#{@name}' with call chain started at #{start_time}"
+      old_invoke_with_call_chain(task_args, invocation_chain)
+      end_time = Time.now
+      puts "Task '#{@name}' with call chain ended at #{end_time}"
+      puts "Total time '#{@name}' with call chain took: #{TimeHelper.time_difference(start_time, end_time)}"
     end
   end
 end
--- spec/rake_performance_spec.rb
+++ spec/rake_performance_spec.rb
@@ -10,7 +10,8 @@ end
 describe "rake-performance" do
   before :each do
     task = Rake::Task[:do_something_useful]
-    Time.stub(:now).and_return Time.new(1983, 10, 23, 10, 10, 23), Time.new(1983, 10, 23, 11, 11, 28)
+    Time.stub(:now).and_return Time.new(1983, 10, 23, 10, 10, 23), Time.new(1983, 10, 23, 10, 10, 24),
+                               Time.new(1983, 10, 23, 11, 11, 27), Time.new(1983, 10, 23, 11, 11, 28)
     $stdout = StringIO.new
     task.invoke
     @output = $stdout.string
@@ -21,12 +22,24 @@ describe "rake-performance" do
     @output.should include "Task \'do_something_useful\' started at 1983-10-23 10:10:23"
   end
 
+  it "displays the task call chain start time" do
+    @output.should include "Task \'do_something_useful\' with call chain started at 1983-10-23 10:10:24"
+  end
+
   it "displays the task end time" do
     @output.should include "Task \'do_something_useful\' ended at 1983-10-23 11:11:28"
+    end
+
+  it "displays the task call chain end time" do
+    @output.should include "Task \'do_something_useful\' with call chain ended at 1983-10-23 11:11:27"
   end
 
   it "displays a the total time taken" do
-    @output.should include "Total time taken: 01:01:05"
+    @output.should include "Total time \'do_something_useful\' took: 01:01:05"
+  end
+
+  it "displays a the total time taken for task with call chain" do
+    @output.should include "Total time \'do_something_useful\' with call chain took: 01:01:03"
   end
 
   it "invokes the task" do
